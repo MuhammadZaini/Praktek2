954b89019878e1d9b8f9b0b3fa85ff481b75b394
MuhammadZaini <32381102+MuhammadZaini@users.noreply.github.com>
2018-04-03 14:41:19 -0700

    Membuat Attibute dan Behavior

--- src/praktek2/Rectangle.java
+++ src/praktek2/Rectangle.java
@@ -1,4 +1,11 @@
 package praktek2;
 public class Rectangle {
-    
+  double panjang;
+    double lebar;
+
+    void cetakInfo (){
+        System.out.println("====================");
+        System.out.println("panjang  : "+panjang);
+        System.out.println("lebar  : "+lebar);
+        System.out.println("====================");
 }
